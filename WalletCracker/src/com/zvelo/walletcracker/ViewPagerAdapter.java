aea783e150fec0548f89b501d66a5e2494cb74c3
Joshua Rubin <jrubin@zvelo.com>
2011-12-26 08:39:48 -0700

    use the much slicker viewpager for tab switching
    Signed-off-by: Joshua Rubin <jrubin@zvelo.com>

---
+++ WalletCracker/src/com/zvelo/walletcracker/ViewPagerAdapter.java
@@ -0,0 +1,51 @@
+package com.zvelo.walletcracker;
+
+import java.util.ArrayList;
+
+import com.viewpagerindicator.TitleProvider;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v13.app.FragmentPagerAdapter;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter implements TitleProvider {
+  private final Context _context;
+  private final ArrayList<TabInfo> _tabs = new ArrayList<TabInfo>();
+
+  static final class TabInfo {
+    private final String _title;
+    private final Class<?> _class;
+    private final Bundle _args;
+
+    TabInfo(String title, Class<?> clss, Bundle args) {
+      _title = title;
+      _class = clss;
+      _args = args;
+    }
+  }
+
+  public ViewPagerAdapter(Activity activity) {
+    super(activity.getFragmentManager());
+    _context = activity;
+  }
+
+  public void addTab(String title, Class<?> clss, Bundle args) {
+    TabInfo info = new TabInfo(title, clss, args);
+    _tabs.add(info);
+  }
+
+  @Override public int getCount() {
+    return _tabs.size();
+  }
+
+  @Override public Fragment getItem(int position) {
+    TabInfo info = _tabs.get(position);
+    return Fragment.instantiate(_context, info._class.getName(), info._args);
+  }
+
+  @Override public String getTitle(int position) {
+    return _tabs.get(position)._title;
+  }
+}
--- WalletCracker/src/com/zvelo/walletcracker/WalletCrackerMain.java
+++ WalletCracker/src/com/zvelo/walletcracker/WalletCrackerMain.java
@@ -1,115 +1,34 @@
 package com.zvelo.walletcracker;
 
-import java.util.ArrayList;
+import com.viewpagerindicator.TitlePageIndicator;
 
 import android.app.ActionBar;
-import android.app.ActionBar.Tab;
 import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentTransaction;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.support.v13.app.FragmentPagerAdapter;
 import android.util.Log;
 
 public class WalletCrackerMain extends Activity {
   protected final String TAG = this.getClass().getSimpleName();
   protected final String RESTORE = ", can restore state";
-  protected ViewPager _pager;
-  protected TabsAdapter _tabsAdapter;
-
-  public static class TabsAdapter
-    extends FragmentPagerAdapter
-    implements ViewPager.OnPageChangeListener, ActionBar.TabListener
-  {
-    private final Context _context;
-    private final ActionBar _actionBar;
-    private final ViewPager _viewPager;
-    private final ArrayList<TabInfo> _tabs = new ArrayList<TabInfo>();
-
-    static final class TabInfo {
-      private final Class<?> _class;
-      private final Bundle _args;
-  
-      TabInfo(Class<?> clss, Bundle args) {
-        _class = clss;
-        _args = args;
-      }
-    }
-
-    public TabsAdapter(Activity activity, ViewPager pager) {
-      super(activity.getFragmentManager());
-      _context = activity;
-      _actionBar = activity.getActionBar();
-      _viewPager = pager;
-      _viewPager.setAdapter(this);
-      _viewPager.setOnPageChangeListener(this);
-    }
-
-    public void addTab(ActionBar.Tab tab, Class<?> clss, Bundle args) {
-      TabInfo info = new TabInfo(clss, args);
-      tab.setTag(info);
-      tab.setTabListener(this);
-      _tabs.add(info);
-      _actionBar.addTab(tab);
-      notifyDataSetChanged();
-    }
-
-    @Override public int getCount() {
-      return _tabs.size();
-    }
-
-    @Override public Fragment getItem(int position) {
-      TabInfo info = _tabs.get(position);
-      return Fragment.instantiate(_context, info._class.getName(), info._args);
-    }
-
-    @Override public void onTabReselected(Tab tab, FragmentTransaction ft) {
-    }
-
-    @Override public void onTabSelected(Tab tab, FragmentTransaction ft) {
-      Object tag = tab.getTag();
-      for (int i=0; i < _tabs.size(); i++) {
-        if (_tabs.get(i) == tag) {
-          _viewPager.setCurrentItem(i);
-        }
-      }
-    }
-
-    @Override public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-    }
-
-    @Override public void onPageScrollStateChanged(int arg0) {
-    }
-
-    @Override public void onPageScrolled(int arg0, float arg1, int arg2) {
-    }
-
-    @Override public void onPageSelected(int position) {
-      _actionBar.setSelectedNavigationItem(position);
-    }
-  }
 
   @Override public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    setContentView(R.layout.pager);
 
-    _pager = new ViewPager(this);
-    _pager.setId(R.id.pager);
-    setContentView(_pager);
+    getActionBar().setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE);
 
-    final ActionBar actionBar = getActionBar();
-    actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-    //actionBar.setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE | ActionBar.DISPLAY_SHOW_HOME);
-    actionBar.setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE);
+    ViewPagerAdapter adapter = new ViewPagerAdapter(this);
+    adapter.addTab(getString(R.string.data), DataFragment.class, null);
+    adapter.addTab(getString(R.string.about), AboutFragment.class, null);
 
-    _tabsAdapter = new TabsAdapter(this, _pager);
-    _tabsAdapter.addTab(actionBar.newTab().setText(R.string.data), DataFragment.class, null);
-    _tabsAdapter.addTab(actionBar.newTab().setText(R.string.about), AboutFragment.class, null);
+    ViewPager pager = (ViewPager) findViewById(R.id.pager);
+    TitlePageIndicator indicator = (TitlePageIndicator) findViewById(R.id.titles);
+    pager.setAdapter(adapter);
+    indicator.setViewPager(pager);
 
     // savedInstanceState could be null
     if (savedInstanceState != null) {
-      actionBar.setSelectedNavigationItem(savedInstanceState.getInt("tab", 0));
       // TODO
     }
   }
@@ -146,7 +65,6 @@ public class WalletCrackerMain extends Activity {
 
   @Override protected void onSaveInstanceState(Bundle outState) {
     super.onSaveInstanceState(outState);
-    outState.putInt("tab", getActionBar().getSelectedNavigationIndex());
     // TODO
     Log.i(TAG, "onSaveInstanceState");
   }
