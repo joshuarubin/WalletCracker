e9ff1284522e37b87db720f58fbe88fcdc02bbb8
Joshua Rubin <joshua@rubixconsulting.com>
2012-01-05 10:47:54 -0700

    slight performance bumps. show more data in about and licenses. linted.
    Signed-off-by: Joshua Rubin <joshua@rubixconsulting.com>

--- src/com/rubixconsulting/walletcracker/LicensesMain.java
+++ src/com/rubixconsulting/walletcracker/LicensesMain.java
@@ -3,15 +3,21 @@ package com.rubixconsulting.walletcracker;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Calendar;
 
 import com.google.android.apps.analytics.easytracking.TrackedActivity;
 
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
 public class LicensesMain extends TrackedActivity {
-  protected final String TAG = this.getClass().getSimpleName();
+  static protected final String TAG = LicensesMain.class.getSimpleName();
+  static protected final Integer THIS_YEAR = Calendar.getInstance().get(Calendar.YEAR);
+  static private Boolean lock = false;
+  static private String versionName, licenseText;
 
   @Override public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -19,11 +25,16 @@ public class LicensesMain extends TrackedActivity {
     setContentView(R.layout.licenses);
 
     TextView licenseView = (TextView) findViewById(R.id.licenseCopy);
-    licenseView.setText(getTextResource(R.raw.licenses));
+    synchronized(lock) {
+      if (licenseText == null) {
+        setLicenseText();
+      }
+      licenseView.setText(licenseText);
+    }
   }
 
-  private String getTextResource(int resourceId) {
-    BufferedReader r = new BufferedReader(new InputStreamReader(getResources().openRawResource(resourceId)));
+  private void setLicenseText() {
+    BufferedReader r = new BufferedReader(new InputStreamReader(getResources().openRawResource(R.raw.licenses)));
 
     String line;
     StringBuilder sb = new StringBuilder();
@@ -40,7 +51,17 @@ public class LicensesMain extends TrackedActivity {
       Log.e(TAG, "IOException: " + e.getMessage());
     }
 
-    Log.d(TAG, sb.toString());
-    return sb.toString().replace("@@app_name@@", getString(R.string.app_name));
+    if (versionName == null) {
+      try {
+        versionName = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_ACTIVITIES).versionName;
+      } catch (NameNotFoundException e) {
+        versionName = getString(R.string.unknown_version);
+      }
+    }
+
+    licenseText = sb.toString()
+      .replace("@@app_name@@", getString(R.string.app_name))
+      .replace("@@app_version@@", versionName)
+      .replace("@@year@@", THIS_YEAR.toString());
   }
 }
